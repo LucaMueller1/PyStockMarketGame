8a3e4732e9fcb519c976d74de4640ade8ecd21de
Daniel <web.par@t-online.de>
2020-10-25 16:02:42 +0100

    Initial DB Service

---
+++ BackEnd/swagger_server/services/db_service.py
@@ -0,0 +1,40 @@
+import sqlalchemy as sqla
+import bcrypt
+
+class DatabaseConn:
+
+    def __init__(self):
+        self.engine = sqla.create_engine('mysql://pybroker:mSWcwbTpuTv4Liwb@pma.tutorialfactory.org/pybroker', echo=True)
+        self.insert_user("demo@demo.de","test1234")
+        self.check_password( "demo@demo.de", "test1234")
+
+
+    def insert_user(self, username, password):
+        auth_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        """with engine.connect() as con:
+            data = ({"id": 1, "title": "The Hobbit", "primary_author": "Tolkien"},
+                    {"id": 2, "title": "The Silmarillion", "primary_author": "Tolkien"},
+                    )
+
+
+            for line in data:
+                con.execute(statement, **line)"""
+        print(auth_password)
+
+    def check_password(self, email: str, password: str) -> bool:
+        data = ({"password": password})
+        with self.engine.connect() as con:
+            rs = con.execute(sqla.text("SELECT * FROM `users` WHERE `email` = :password"), data)
+            for row in rs:
+                hashAndSalt = row[4]
+                print(hashAndSalt)
+                print(row)
+        valid = bcrypt.checkpw(password.encode('utf8'), hashAndSalt)
+        return valid
+    def check_api_key(self, apikey:str, required_scopes=None):
+     if apikey == "lol123":
+        return {'sub': 'admin'}
+
+    # optional: raise exception for custom error response
+        return None
+db= DatabaseConn()
