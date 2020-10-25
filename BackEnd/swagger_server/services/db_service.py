6b8e0fde4770a7ad65e82a7ec1b36f05de321e3c
Daniel <web.par@t-online.de>
2020-10-25 16:29:30 +0100

    Initial DB Service#3

--- BackEnd/swagger_server/services/db_service.py
+++ BackEnd/swagger_server/services/db_service.py
@@ -1,35 +1,28 @@
 import sqlalchemy as sqla
 import bcrypt
+import auth_key
 
 class DatabaseConn:
 
     def __init__(self):
         self.engine = sqla.create_engine('mysql://pybroker:mSWcwbTpuTv4Liwb@pma.tutorialfactory.org/pybroker', echo=True)
         self.insert_user("demo@demo.de","test1234")
-        self.check_password( "demo@demo.de", "test1234")
+        print(self.check_password( "demo@demo.de", "test1234"))
 
 
     def insert_user(self, username, password):
         auth_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-        """with engine.connect() as con:
-            data = ({"id": 1, "title": "The Hobbit", "primary_author": "Tolkien"},
-                    {"id": 2, "title": "The Silmarillion", "primary_author": "Tolkien"},
-                    )
 
-
-            for line in data:
-                con.execute(statement, **line)"""
-        print(auth_password)
+        print(auth_password.decode('utf8'))
 
     def check_password(self, email: str, password: str) -> bool:
-        data = ({"password": password})
         with self.engine.connect() as con:
-            rs = con.execute(sqla.text("SELECT * FROM `users` WHERE `email` = :password"), data)
+            rs = con.execute(sqla.text("SELECT * FROM `users` WHERE `email` = :mail_address"), mail_address = email)
             for row in rs:
                 hashAndSalt = row[4]
                 print(hashAndSalt)
                 print(row)
-        valid = bcrypt.checkpw(password.encode('utf8'), hashAndSalt)
+        valid = bcrypt.checkpw(password.encode('utf8'), bytes(hashAndSalt, 'utf-8'))
         return valid
-
+    def generate_auth_hash(self, userid: int)-> :
 db= DatabaseConn()
