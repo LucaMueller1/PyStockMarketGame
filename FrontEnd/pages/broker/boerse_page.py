e1bf8c86ffdd448a16f9045edb49b89537822ab2
LucaWeissbeck <62757957+LucaWeissbeck@users.noreply.github.com>
2020-11-02 21:26:20 +0100

    Changed all variables and parts of documentation to english and logical names in boerse_page.py

--- FrontEnd/pages/broker/boerse_page.py
+++ FrontEnd/pages/broker/boerse_page.py
@@ -29,60 +29,56 @@ def run(session_state):
         if session_state.stock_id:
             test_array.append(session_state.stock_id)
         else:
-            test_array.append("")
+            test_array.append(" ")
         # Get the WKN from the User
         st.write("Gib hier eine Wertpapier Kennnummer (WKN) ein, sowie die Anzahl der zu kaufenden Aktien")
-        wkn_user_eingabe = st.selectbox("WKN:", test_array + session_state.stock_names)
-        # Split dat shit
-        if wkn_user_eingabe != "":
-            wkn_user_eingabe_uebergabe = wkn_user_eingabe.split(": ")[1]
+        ticker_code_entry = st.selectbox("WKN:", test_array + session_state.stock_names)
 
-            if wkn_user_eingabe == " ":
-                st.error("Please choose a WKN")
+        if ticker_code_entry != " ":
+            # Splittin the stock ticker from the stock name
+            ticker_code_entry_for_post_request = ticker_code_entry.split(": ")[1]
+
             # Get the quantity from user
-            wkn_anzahl_user = st.number_input("Anzahl:", step = 1.0)
+            ticker_quantity_entry = st.number_input("Anzahl:", step = 1.0)
+            ticker_quantity_entry = int(ticker_quantity_entry)
 
-            # If button is clicked --> Entry submitted
-
+            # If button is clicked --> Entry submitted (quantity & stock ticker)
             if st.button("Eingabe bestaetigen"):
                 session_state.buy_redirect = True
 
             if session_state.buy_redirect:
-                wkn_anzahl_user_eingabe = int(wkn_anzahl_user)
-                st.success("Success")
-
                 # We are only defining the layout here in order to split the page into two parts.
                 # The lefthand part will be filled with the total amount and the "Kaufen" button
                 # Righthand part will be filled with Aktieninfos (Aktienname, Aktienwert, Dividendenrendite, Symbol)
                 col1, col2 = st.beta_columns(2)
 
-                # Aktienwert is calculated by 190 (later changed to chosen Stock from wkn_user_eingabe) times the amount chosen by user
-                aktienwert = 190 * int(wkn_anzahl_user)
+                # Aktienwert is calculated by 190 (later changed to chosen Stock from ticker_code_entry) times the amount chosen by user
+                specific_stock_value = 190 * int(ticker_quantity_entry)
 
                 # Gebuehren is static for now, but will later be fetched from database
-                gebuehren = 9.90
+                purchase_fees = 9.90
 
                 # Dividendenrendite is static for now, but will later be fetched from database
-                dividendenrendite_raw = 0.06
-                dividendenrendite = str(dividendenrendite_raw * 100) + "%"
+                dividend_yield_raw = 0.06
+                dividend_yield = str(dividend_yield_raw * 100) + "%"
 
-                # Add Gebuehren to aktienwert
-                realisierender_kaufwert = str(aktienwert + gebuehren) + "€"
+                # Add Gebuehren to specific_stock_value
+                purchase_value = str(specific_stock_value + purchase_fees) + "€"
 
                 # Place price information and "Kaufen" button on the left side
                 with col1:
                     st.subheader("Verkaufsübersicht")
                     st.write("----------------------")
-                    st.write("Aktienkaufpreis:", aktienwert)
-                    st.write("Gebühren:", "", gebuehren)
+                    st.write("Aktienkaufpreis:", specific_stock_value)
+                    st.write("Gebühren:", "", purchase_fees)
                     st.write("----------------------")
                     st.subheader("Kaufpreis:")
-                    st.title(realisierender_kaufwert)
+                    st.title(purchase_value)
 
                     if st.button("Buy"):
                         # send post request to DB
-                        response = requests_server.post_transaction(session_state.auth_key, wkn_user_eingabe_uebergabe,
-                                                                        wkn_anzahl_user_eingabe, transactionType="buy")
+                        response = requests_server.post_transaction(session_state.auth_key, ticker_code_entry_for_post_request,
+                                                                        ticker_quantity_entry, transactionType="buy")
 
                         st.write("Du kannst deine gekauften Aktien nun im Depot beobachten.")
                         sleep(2)
@@ -93,7 +89,6 @@ def run(session_state):
                         # This is where the actual buy happens
                         # Once connected to BE: Get Price of chosen stock & fee --> Calculate end price
 
-
                 # Place Stock information on the right side
                 with col2:
                     st.markdown("""
@@ -101,7 +96,7 @@ def run(session_state):
                                     <h2>Aktieninformationen</h2>
                                     <p>Aktienname: <b>Adidas</b></p>
                                     <p>Aktienwert: <b>""" + "190" + """<b></p>
-                                    <p>Dividendenrendite: <b>""" + dividendenrendite + """<b></p>
+                                    <p>Dividendenrendite: <b>""" + dividend_yield + """<b></p>
                                     <img class = "circle_and_center" src = "https://logo.clearbit.com/apple.com">
                                     </div>
                                     """
