c3a31de7bce86a49c403372c4738e7c75d05a10b
LucaWeissbeck <62757957+LucaWeissbeck@users.noreply.github.com>
2020-11-03 14:12:00 +0100

    Translated Frontend user page in boerse_page.py and boerse_page_sell.py into English.

--- FrontEnd/pages/broker/boerse_page_sell.py
+++ FrontEnd/pages/broker/boerse_page_sell.py
@@ -17,56 +17,56 @@ def run(session_state):
 
     local_css("FrontEnd/css/style.css")
 
-    st.header("Börse")
-    st.subheader("Willkommen zur Börse. Hier kannst du Wertpapiere kaufen oder deine vorhandenen Wertpapiere verkaufen.")
+    st.header("Broker")
+    st.subheader("Welcome to your personalised broker. Here you can buy and sell your stocks.")
 
     # switch between BUY and SELL
-    mode_switch = st.radio("Bitte wähle aus, ob du Werpapiere kaufen oder verkaufen möchtest", ("Kaufen", "Verkaufen"))
+    mode_switch = st.radio("Please choose whether you want to buy or sell stocks", ("Buy", "Sell"))
 
-    if mode_switch == "Verkaufen":
+    if mode_switch == "Sell":
 
-        st.write("Wähle deine Aktie aus, die du verkaufen möchtest.")
+        st.write("Please choose the stock, that you want to sell.")
 
         # Get the stock from user, which he / she wants to sell
-        wkn_verkaufen_user_eingabe = st.selectbox("WKN:",[" "] + session_state.stock_names)
-        if wkn_verkaufen_user_eingabe != " ":
-            wkn_verkaufen_uebergabe = wkn_verkaufen_user_eingabe.split(": ")[1]
+        ticker_code_entry = st.selectbox("WKN:",[" "] + session_state.stock_names)
+        if ticker_code_entry != " ":
+            ticker_code_entry_for_post_request = ticker_code_entry.split(": ")[1]
 
             # Connect to Databse later!!!
-            quantity_of_stock_im_Depo = 6
+            quantity_of_specific_stock_in_depot = 6
 
             # By standard all stocks are sold, UNLESS user ticks this box:
-            if st.checkbox("Alle Aktien verkaufen", value=True):
+            if st.checkbox("Sell all stocks", value=True):
                 # Set the Value of quantity to the amount user has in Depot
                 # For now this is STATIC
                 stock_quantity = 6
 
             else:
-                # Die Seite wird aufgeteilt, sodass der User auf der rechten Seite eine Angabe hat, wie viele Aktien er von der ausgewählten Aktie im Depot hat
+                # Site is split Die Seite wird aufgeteilt, sodass der User auf der rechten Seite eine Angabe hat, wie viele Aktien er von der ausgewählten Aktie im Depot hat
                 col1, col2 = st.beta_columns((4, 1))
 
-                # Auswahl der Quantität
+                # Selection of quantity
                 with col1:
                     # Sollte der User eine genau Anzahl eingeben wollen, so kann er hier "textfeld" auswaehlen
-                    input_methode = st.radio("Inputmethode", ("Slider", "Textfeld"))
+                    quantity_input_method_choice = st.radio("Input method", ("Slider", "Textfield"))
 
                     # Slider
-                    if input_methode == "Slider":
-                        stock_quantity = st.slider("Wähle aus, wie viele Aktien du verkaufen möchtest", 1, 6)
+                    if quantity_input_method_choice == "Slider":
+                        stock_quantity = st.slider("Please choose the quantity of stocks", 1, 6)
 
                     # Textfeld & Button
-                    if input_methode == "Textfeld":
-                        stock_quantity_raw = st.number_input("Oder gib die genaue Anzahl hier ein:", min_value = 1, step = 1)
+                    if quantity_input_method_choice == "Textfield":
+                        stock_quantity_raw = st.number_input("Please choose the quantity of stocks", min_value = 1, step = 1)
                         stock_quantity = 1
-                        if st.button("Bestätigen"):
+                        if st.button("Apply"):
                             stock_quantity = int(stock_quantity_raw.title())
 
                 # Anzeige der Quantitaät der ausgewaählten Aktie im Depot
                 with col2:
                     st.markdown("""
                             <div class="greyish padding">
-                            <h4>Quantität im Depot:</h4>
-                            <h1>""" + str(quantity_of_stock_im_Depo) + """</h1>
+                            <h4>Quantity in Depot</h4>
+                            <h1>""" + str(quantity_of_specific_stock_in_depot) + """</h1>
                             </div>
                             """
                                 , unsafe_allow_html=True)
@@ -77,34 +77,34 @@ def run(session_state):
             # Vorbereiten der Variablen (Verkaufspreis, Ordergebühren)
 
             # Verkaufspreis hier mit STATISCH adidas --> VON DB holen die Aktie!
-            aktienverkaufswert = stock_quantity * 169.88
+            stock_sell_value_price = stock_quantity * 169.88
 
-            # verkaufsgebuehren, hier statisch --> von DB holen!
-            verkaufsgebuehren = 9.90
+            # selling_fees, hier statisch --> von DB holen!
+            selling_fees = 9.90
 
             # Verkaufspreis mit Abzug der Gebuehren:
-            realisierender_verkaufswert = str(aktienverkaufswert - verkaufsgebuehren) + "€"
+            total_sell_value = str(stock_sell_value_price - selling_fees) + "€"
 
-            # Dividendenrendite Standardmässig 0 setzen
-            dividendenrendite2_raw = 0.06
-            dividendenrendite2 = str(dividendenrendite2_raw * 100) + "%"
+            # Dividendenrendite Standardmässig 0 setzen 
+            dividend_yield_raw = 0.06
+            dividend_yield = str(dividend_yield_raw * 100) + "%"
 
             # Auflistung Verkaufspreis mit Ordergebühren
             with col1:
-                st.subheader("Verkaufsübersicht")
+                st.subheader("Sell - Overview")
                 st.write("----------------------")
-                st.write("Aktienverkaufswert:", aktienverkaufswert)
-                st.write("Gebühren: -", verkaufsgebuehren)
+                st.write("Stock value:", stock_sell_value_price)
+                st.write("Selling fees: -", selling_fees)
                 st.write("----------------------")
-                st.subheader("Verkaufswert:")
-                st.title(realisierender_verkaufswert)
+                st.subheader("Total selling value:")
+                st.title(total_sell_value)
 
                 # Sell button
-                if st.button("Verkaufen"):
-                    st.subheader("Verkauft")
+                if st.button("Sell"):
+                    st.subheader("Sold")
 
                     # Talk to Backend
-                    response = requests_server.post_transaction(session_state.auth_key, wkn_verkaufen_uebergabe,
+                    response = requests_server.post_transaction(session_state.auth_key, ticker_code_entry_for_post_request,
                                                                             stock_quantity, transactionType="sell")
 
 
@@ -112,15 +112,15 @@ def run(session_state):
             with col2:
                 st.markdown("""
                                             <div class="greyish padding">
-                                            <h2>Aktieninformationen</h2>
-                                            <p>Aktienname: <b>Adidas</b></p>
-                                            <p>Aktienwert: <b>""" + "190" + """<b></p>
-                                            <p>Dividendenrendite: <b>""" + dividendenrendite2 + """<b></p>
+                                            <h2>Stock information</h2>
+                                            <p>Stock name: <b>Adidas</b></p>
+                                            <p>Stock value: <b>""" + "190" + """<b></p>
+                                            <p>Dividendyield: <b>""" + dividend_yield + """<b></p>
                                             <img class = "circle_and_center" src = "https://logo.clearbit.com/adidas-group.com">
                                             </div>
                                             """
                             , unsafe_allow_html=True)
 
-    elif mode_switch == "Kaufen":
+    elif mode_switch == "Buy":
         session_state.page = "boerse"
         st.experimental_rerun()
