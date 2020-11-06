4c4e2535741c95426c9f2e48cc34f51225a32492
janniksinz <j.sinz@gmx.de>
2020-11-06 23:35:45 +0100

    implemented cronjob / scheduler for daily stock data (incomplete)

---
+++ BackEnd/swagger_server/services/schedule_service.py
@@ -0,0 +1,20 @@
+import schedule
+import time
+from swagger_server.services.finance import finance_data
+from swagger_server.services import trading_service
+from swagger_server.services.db_service import DatabaseConn
+
+
+conn = DatabaseConn()
+users = conn.get_all_users()
+stocks = []
+
+for user in users:
+    portfolio = trading_service.get_portfolio_positions()
+    for position in portfolio:
+        symbol = position.symbol
+        # make sure double data is not collected
+        if symbol not in stocks:
+            stocks.append(symbol)
+            schedule.every(24).hour.do(finance_data.insert_stock_history_from_yfinance_to_db(symbol))
+
