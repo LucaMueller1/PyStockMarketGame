e9e6cbb78b6aab336d9af47a9bf38f1ffed22ee0
janniksinz <j.sinz@gmx.de>
2020-11-06 21:13:20 +0100

    added trading_service test

---
+++ BackEnd/swagger_server/test/test_trading_service.py
@@ -0,0 +1,10 @@
+from swagger_server.models.stock_description import StockDescription
+from swagger_server.models.stock_value import StockValue
+from swagger_server.models.auth_key import AuthKey
+from swagger_server.services import trading_service
+
+description = StockDescription("AMZN", None, None, None, None, None, None, None, None, None, None)
+auth_key = AuthKey(None, "Aj7LrIzhhciVLS1JgNCp9rAfCmseDOOgFf3mwGj9vMFYVZQ3aZ2sLB7jYW1x3B0VwWUnAYfjG846enNzVotw2ODixHqJz9JR0RpzwpmbZy44g39ci1vgaG9S9ObCFGKj", None)
+
+transaction = trading_service.buy_stock(auth_key, description, 10)
+print(transaction)
