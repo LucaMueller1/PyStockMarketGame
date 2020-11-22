66cf270ca4439acdd789d86e35c05ce5c37457f6
REIGNjs <lucamueller32@gmail.com>
2020-11-22 13:02:47 +0100

    Added doc string and correct database method in auth_service.py

--- BackEnd/swagger_server/services/auth_service.py
+++ BackEnd/swagger_server/services/auth_service.py
@@ -1,13 +1,25 @@
-from swagger_server.services.db_service import DatabaseConn;
+"""
+desc: An authentication service that validates or invalidates a given API-Key
+author: Luca Mueller
+date: 2020-11-21
+"""
+
+from swagger_server.controllers import staticglobaldb
 from swagger_server.models.auth_key import AuthKey
 
-"""
-MISSING DOC STRING DOCUMENTATION
-"""
 
 def api_key_auth(apikey: str, required_scopes=None):
-    conn = DatabaseConn()
-    auth_key = conn.check_auth_hash(apikey)
+    """Checks database for given API key and returns None if no valid Key is found
+
+    :param apikey: API-Key provided by user
+    :type apikey: str
+    :param required_scopes: What authorization scope is needed
+    :type required_scopes: object
+
+    :rtype: Dict
+    :test Correct: The provided apikey belongs to a logged-in user in the database and the method will return a dict. Incorrect: The provided API-Key does not exist in the database, thus the user will not be authorized and None is returned
+    """
+    auth_key = staticglobaldb.dbconn.check_auth_hash(apikey)
 
     if auth_key is None:
         print("Auth NOT accepted")
